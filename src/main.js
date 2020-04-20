8c2ff13d4d62b69566e218c2656973c27137bad6
xiuxiu <2457982187@qq.com>
2020-04-20 20:35:48 +0800

    目录结构调整

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+// 加载index.less
+import './styles/index.less'
 Vue.config.productionTip = false
 
 // 通过render方法把根组件渲染到#app节点。
